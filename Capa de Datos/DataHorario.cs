154b433e2e37ddfa6e7d5f0004cc8069574d602a
Camila Martinez Bedoya <camilamarb98@gmail.com>
2021-06-27 00:24:33 -0500

    Reporte Horarios mas populares segun fecha

--- Capa de Datos/DataHorario.cs
+++ Capa de Datos/DataHorario.cs
@@ -34,46 +34,25 @@ namespace CapaDatos
             return contexto.Horarios.ToList<Horario>();
         }
 
-        public List<EntityHorarioMasReservado> HorarioMasFreuentado()
+        public List<EntityHorarioMasReservado> HorarioMasFrecuentado(DateTime fechaInicio, DateTime fechaFin)
         {
             List<EntityHorarioMasReservado> objNum = new List<EntityHorarioMasReservado>();
-            List<EntityHorarioMasReservado> owo = new List<EntityHorarioMasReservado>();
             ShamaticaStudioEntities contexto = new ShamaticaStudioEntities();
-            var outcome = from reserva in contexto.Reservas
-                          group reserva by reserva.codigo_horario into NuevoGrupo
-                          orderby NuevoGrupo.Key
-                          select NuevoGrupo;
-            /*foreach (var reservass in outcome)
-            {
-                EntityHorarioMasReservado obj = new EntityHorarioMasReservado();
-                obj.horario = (int)reservass.Key;
-                obj.cantidadhorariosrepetidos = reservass.Count();
-                objNum.Add(obj);
-            }*/
-           
-            var lmaoo = from horario in contexto.Horarios
+          
+            var result = from horario in contexto.Horarios
                           join reserva in contexto.Reservas on horario.id_horario equals reserva.codigo_horario
+                          where reserva.fecha_reserva >= fechaInicio && reserva.fecha_reserva <= fechaFin
                           group horario by horario.hora_reserva into NuevoGrupo
                          // orderby NuevoGrupo.Key
                           select NuevoGrupo;
-            foreach(var abcde in lmaoo)
+            foreach(var abcde in result)
             {
                 EntityHorarioMasReservado obj = new EntityHorarioMasReservado();
                 obj.horario = abcde.Key;
                 obj.cantidadhorariosrepetidos = abcde.Count();
                 objNum.Add(obj);
             }
-
             return objNum;
-
-        }
-       /* public string RenombrarElementos()
-        {
-            HorarioMasFreuentado();
-            ShamaticaStudioEntities contexto = new ShamaticaStudioEntities();
-            var result = (from horario in contexto.Horarios
-                          where horario.id_horario == HorarioMasFreuentado(). )
-        }*/
-        
+        }        
     }
 }
